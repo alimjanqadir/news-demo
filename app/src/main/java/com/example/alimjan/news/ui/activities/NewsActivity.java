c8a75c32dd0c7cfb135774a019f054ba216251c3
Alimjan Qadir <alimjanqadir@qq.com>
2018-09-24 17:46:50 +0800

    Splash screen added.

--- app/src/main/java/com/example/alimjan/news/ui/activities/NewsActivity.java
+++ app/src/main/java/com/example/alimjan/news/ui/activities/NewsActivity.java
@@ -13,6 +13,7 @@ public class NewsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
